3140f010a4561ff6486584d04b8955f948e3655f
Iker Ibarguren <ikerib@gmail.com>
2015-02-25 16:54:00 +0100

    shooting

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject PrefBala;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			GameObject bala = (GameObject)GameObject.Instantiate(PrefBala); 
+			Rigidbody balaFisicas = (Rigidbody) bala.GetComponent<Rigidbody>();
+			balaFisicas.AddForce(new Vector3(0,130,-400));
+		}
+	}
+}
